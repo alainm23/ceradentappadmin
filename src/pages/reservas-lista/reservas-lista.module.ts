e7da1ed97c0590f3006fab6cae862f9163a06ab6
Alain <alain@pop-os.localdomain>
2021-07-25 09:04:14 -0500

    *

--- src/pages/reservas-lista/reservas-lista.module.ts
+++ src/pages/reservas-lista/reservas-lista.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ReservasListaPage } from './reservas-lista';
+import { OrderModule } from 'ngx-order-pipe';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ReservasListaPage } from './reservas-lista';
   ],
   imports: [
     IonicPageModule.forChild(ReservasListaPage),
+    OrderModule
   ],
 })
 export class ReservasListaPageModule {}
